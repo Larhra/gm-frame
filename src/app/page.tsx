21685c827526cd50f1bd7e4ccd850e623cd25b84
Larhra <agbajeaminah@yahoo.com>
2024-03-25 18:28:38 -0700

    third commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { getFrameMetadata } from '@coinbase/onchainkit';
 import type { Metadata } from 'next';
 
 
-const FRAMES_URL = process.env.FRAMES_URL || "https://localhoost:3000"
+const FRAMES_URL = 'https://gm-frame-seven.vercel.app/'
 const imageUrl = new URL("/og", FRAMES_URL).href
 const postUrl = new URL("/frame", FRAMES_URL).href
 
